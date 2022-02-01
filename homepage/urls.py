1b3c0d8b3f9e6f83b1ed09096eb074d31361740d
coder-dipesh <dipeshsiwakoti112@gmail.com>
2022-02-01 17:30:01 +0545

    Everything about admin Category Section -Add -View  -Update -Delete :V::heavy_check_mark:

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,17 +4,7 @@ from . import views
 urlpatterns = [
     # path('error404',views.error404, name='error404'),
     path('',views.homepage, name='homepage'),
-
-    # path('',),
-    # path('',),
-    # path('',),
     path('about',views.about, name='about'),
-
-     # path('',),
-    # path('',),
-    # path('',),
     path('service',views.service,name='service'),
-    path('footer',views.footer,name='footer')
-
-
+    path('footer',views.footer,name='footer'),
 ]
