87d7146f59a2114ba70dc0b87e29ab709ca134dc
Karma Gurung <63505288+KarmaGurung75@users.noreply.github.com>
2022-02-01 19:50:03 +0545

    Home Page

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -40,10 +40,40 @@ $('.slider').slick({
 	]
   });
 
+  var multipleCardCarousel = document.querySelector(
+  "#carouselExampleControls3");
+if (window.matchMedia("(min-width: 768px)").matches) {
+  var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+    interval: false,
+  });
+  var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+  var cardWidth = $(".carousel-item").width();
+  var scrollPosition = 0;
+  $("#carouselExampleControls3 .carousel-control-next").on("click", function () {
+    if (scrollPosition < carouselWidth - cardWidth * 4) {
+      scrollPosition += cardWidth;
+      $("#carouselExampleControls3 .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+  $("#carouselExampleControls3 .carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition -= cardWidth;
+      $("#carouselExampleControls3 .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+} else {
+  $(multipleCardCarousel).addClass("slide");
+}
+
 // $(".hover").mouseleave(
 // 	function () {
 // 	  $(this).removeClass("hover");
 // 	}
 //   );
-  
-
+ 
