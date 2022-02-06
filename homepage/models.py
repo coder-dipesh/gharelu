5438413af7e77edbe48f6045a5b5f179fc36aa8e
pooja9009 <miss.poojarijal@gmail.com>
2022-02-06 15:37:31 +0545

    review

--- homepage/models.py
+++ homepage/models.py
@@ -2,9 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-class Feedback(models.Model):
-    name = models.CharField(max_length=100, validators=[validators.MinLengthValidator(2)])
-    product_feedback = models.TextField()
-
-    def __str__(self):
-        return self.product_feedback
--- homepage/urls.py
+++ homepage/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('about',views.about, name='about'),
     path('service',views.service,name='service'),
 
-    path('footer',views.footer,name='footer'),
+    # path('footer',views.footer,name='footer'),
 
     path('review',views.review,name='review'),
 
